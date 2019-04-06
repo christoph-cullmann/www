875f702a786a771db915b9b37bd45e191c2e79af
Christoph Cullmann <cullmann@kde.org>
2019-04-06 17:17:57 +0200

    update scripty

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# failures are evil
+set -e
+
+# update our clone
+git submodule update --init --recursive
+git submodule update --recursive --remote
+git pull
+
+# rerun hugo generator
+./hugo
