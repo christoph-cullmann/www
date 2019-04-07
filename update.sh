6faa22033d0988231f41b58c6b8a9e3aa5c97140
Christoph Cullmann <cullmann@kde.org>
2019-04-07 16:52:09 +0200

    update to right version

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ set -e
 # update our clone
 git submodule update --init --recursive
 git submodule foreach git pull origin master
-git pull --recurse-submodules
+git pull
 
 # rerun hugo generator
 # export the GIT env vars we need to have links in the footer
