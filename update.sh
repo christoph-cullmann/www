d4cc774f0c90a15ca4d1e6a068bdb96b29b7ab1b
Christoph Cullmann <cullmann@kde.org>
2019-04-08 16:45:33 +0200

    be more silent

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@
 set -e
 
 # update our clone
-git submodule update --init --recursive
-git submodule foreach git pull origin master
-git pull
+git submodule update --init --recursive &> /dev/null
+git submodule foreach git pull -q origin master &> /dev/null
+git pull -q &> /dev/null
 
 # rerun hugo generator
 # export the GIT env vars we need to have links in the footer
