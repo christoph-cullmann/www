560af2c95eda4c20acff5657e54d36e6241796a1
Christoph Cullmann <cullmann@kde.org>
2019-04-13 23:20:08 +0200

    minify the stuff

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ git pull -q &> /dev/null
 # export the GIT env vars we need to have links in the footer
 export GIT_COMMIT_SHA=`git rev-parse --verify HEAD`
 export GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD`
-./hugo
+./hugo --minify
 
 # copy .htaccess
 cp -f static/.htaccess public
