d9364d2799a952d018d7ee9ab27beaa7db561482
Christoph Cullmann <cullmann@kde.org>
2022-10-21 22:04:10 +0200

    ensure really no cookies

--- update-theme.sh
+++ update-theme.sh
@@ -2,4 +2,5 @@ rm -rf themes
 mkdir themes
 git clone https://github.com/adityatelange/hugo-PaperMod themes/PaperMod --depth=1
 rm -rf themes/PaperMod/.git*
+sed -i s@localStorage.setItem@//localStorage.setItem@g themes/PaperMod/layouts/partials/footer.html
 git add themes
