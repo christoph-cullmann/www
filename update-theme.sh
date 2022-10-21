9e3457474b73741354cf222eb419e4733c2aac20
Christoph Cullmann <cullmann@kde.org>
2022-10-21 21:05:46 +0200

    use different theme that is a lot slimer

--- update-theme.sh
+++ update-theme.sh
@@ -1,5 +1,5 @@
 rm -rf themes
 mkdir themes
 git clone https://github.com/adityatelange/hugo-PaperMod themes/PaperMod --depth=1
-rm -rf themes/PaperMod/.git
+rm -rf themes/PaperMod/.git*
 git add themes
