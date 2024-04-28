8848ad1297a38e97adf09c9213733be56cf61b08
Christoph Cullmann <cullmann@kde.org>
2024-04-28 17:34:11 +0200

    remove git hacks

--- server.sh
+++ server.sh
@@ -3,9 +3,5 @@
 # failures are evil
 set -e
 
-# export the GIT env vars we need to have links in the footer
-export GIT_COMMIT_SHA=`git rev-parse --verify HEAD`
-export GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD`
-
 # run hugo in local server mode, show all stuff
 exec hugo server -D --buildFuture
