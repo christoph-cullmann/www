e77051ccc4b47951bfa4fde2be436b1bb2fb113b
Christoph Cullmann <cullmann@kde.org>
2024-04-28 17:33:09 +0200

    use https://github.com/nunocoracao/blowfish.git

--- update-theme.sh
+++ update-theme.sh
@@ -1,7 +1,5 @@
 rm -rf themes
 mkdir themes
-git clone https://github.com/adityatelange/hugo-PaperMod themes/PaperMod --depth=1
-rm -rf themes/PaperMod/.git*
-sed -i 's@localStorage.setItem@//localStorage.setItem@g' themes/PaperMod/layouts/partials/footer.html
-sed -i 's@safeURL@safeURL | absURL@g' themes/PaperMod/layouts/_default/_markup/render-image.html
+git clone https://github.com/nunocoracao/blowfish.git themes/blowfish --depth=1
+rm -rf themes/blowfish/.git*
 git add themes
