015090549306ba6fd0c84a874b58559c4a1fa8fb
Christoph Cullmann <cullmann@kde.org>
2024-04-29 22:50:58 +0200

    update theme without example page

--- update-theme.sh
+++ update-theme.sh
@@ -1,5 +1,5 @@
 rm -rf themes
 mkdir themes
 git clone https://github.com/nunocoracao/blowfish.git themes/blowfish --depth=1
-rm -rf themes/blowfish/.git*
+rm -rf themes/blowfish/.git* themes/blowfish/exampleSite
 git add themes
