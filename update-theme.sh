5992869fd3209c91e0e4fd1a876e3710895aad13
Christoph Cullmann <christoph@cullmann.dev>
2025-07-19 21:46:51 +0200

    try https://github.com/athul/archie.git

--- update-theme.sh
+++ update-theme.sh
@@ -1,6 +1,6 @@
 rm -rf themes
 mkdir themes
 cd themes
-git clone --depth=1 https://github.com/janraasch/hugo-bearblog.git
-rm -rf hugo-bearblog/.git* hugo-bearblog/exampleSite
+git clone --depth=1 https://github.com/athul/archie.git
+rm -rf */.git*
 git add .
